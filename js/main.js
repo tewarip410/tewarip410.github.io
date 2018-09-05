2b4a8d4b2773efda76f05c313edcf2daad3d32d0
Pooja Tewari <poojatewrai410@gmail.com>
2018-09-05 08:51:02 -0400

    pooja

--- js/main.js
+++ js/main.js
@@ -24,7 +24,31 @@ $('#nav').affix({
       }
 });	
 
-	
+	// skills chart
+	$(document).ready(function(e) {
+	//var windowBottom = $(window).height();
+	var index=0;
+	$(document).scroll(function(){
+		var top = $('#skills').height()-$(window).scrollTop();
+		console.log(top)
+		if(top<-300){
+			if(index==0){	
+			
+				$('.chart').easyPieChart({
+					easing: 'easeOutBounce',
+					onStep: function(from, to, percent) {
+						$(this.el).find('.percent').text(Math.round(percent));
+					}
+				});
+			
+				}
+			index++;
+		}
+	})
+	//console.log(nagativeValue)
+	});
+
+
   	// Portfolio isotope filter
     $(window).load(function() {
         var $container = $('.portfolio-items');
@@ -53,13 +77,21 @@ $('#nav').affix({
 
     });
 	
-
-    // Nivo Lightbox 
-    $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',  
-            keyboardNav: true,                            
-        });
- 
+	  	
+    // CounterUp
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){	
+			$('span.count').counterUp({
+					delay: 10, // the delay time in ms
+			time: 1500 // the speed time in ms
+			});
+		}
+	});
+	
+  	// Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
 
 }());
 
